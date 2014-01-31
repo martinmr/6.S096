6eeda68eee6f141d9acb11aa475f83c2d9e84491
kolya <perunov@mit.edu>
2014-01-30 23:29:56 -0500

    Created header file for Runge-Kutta 4-th order integrator

---
+++ include/nbody/RK4Integrator.h
@@ -0,0 +1,27 @@
+#ifndef _NBODY_RK4_INTEGRATOR_H
+#define _NBODY_RK4_INTEGRATOR_H
+
+#include <nbody/Integrator.h>
+#include <nbody/System.h>
+
+namespace nbody {
+    /* Runge-Kutta method of solving differential equation:
+     * \dot{y} = f(t, y), y(t_0) = y_0.
+     * For time step h > 0 we find:
+     * y_{n+1} = y_n + 1/6 * ( k_1 + 2*k_2 + 2*k_3 + k_4 )
+     * t_{n+1} = t_n + h, where increments k_i (i = 1, 2, 3, 4) are
+     * k_1 = f( t_n, y_n);
+     * k_2 = f( t_n + 1/2 * h, y_n + h/2 * k_1 );
+     * k_3 = f( t_n + 1/2 * h, y_n + h/2 * k_2 );
+     * k_4 = f( t_n + h, y_n + h *k_3 ).
+     * For nbody system y = { pos[j], vel[j] }, f = { vel[j], acc[j] }.
+    */
+    class RK4Integrator : public Integrator {
+    public:
+        void integrateSystem( System &s, float dt ) const;
+    };
+
+}
+
+#endif //_NBODY_SIMPLE_INTEGRATOR_H
+
