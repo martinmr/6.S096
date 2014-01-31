16c9ade6a78364171063a8474f4d83d7b4103ac9
kolya <perunov@mit.edu>
2014-01-30 23:32:34 -0500

    Created RK4Integrator.cpp

---
+++ src/RK4Integrator.cpp
@@ -0,0 +1,23 @@
+#include <nbody/RK4Integrator.h>
+#include <nbody/System.h>
+#include <nbody/Vector3.h>
+
+namespace nbody {
+
+    void SimpleIntegrator::integrateSystem( System &s, float dt ) const{
+        Vector3f r, v, a;
+        for( size_t i = 0; i < s.nBodies(); ++i ) {
+            r = s.body(i).position();
+            v = s.body(i).velocity();
+            a = s.body(i).force();
+
+            v = v + ( a * dt );
+            v = v * s.dampingFactor();
+            r = r + v * dt;
+
+            s.body(i).position() = r;
+            s.body(i).velocity() = v;
+        }
+    }
+
+} //namespace nbody
